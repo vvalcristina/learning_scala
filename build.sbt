bad2d7604374a42ff5ef3ef1ba367236eeed8a93
Valeria Silva <silvvavaleria@gmail.com>
2020-02-21 09:09:50 -0300

    Primeiro Scala- calculadora, utilizando funções anônimas

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "learning_scala"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % "test"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,34 @@
+object Main {
+  //def : declaração de função -> def nome(parâmetros)=> retorno
+  //não precisa utilizar return
+  //Unit -> função não retorna nada
+  //Scala permite a utilização de funções anônimas
+  def Calc(callback: (Int, Int) => Int, x:Int, y:Int): Unit ={
+    println(callback(x,y))
+  }
+  def contaUmDez(f:)
+
+  def soma (x: Int, y: Int): Int={
+    x + y
+  }
+
+  def multiplicacao (x: Int, y: Int): Int={
+    x * y
+  }
+
+  def divisao (x:Int, y: Int): Int ={
+    x / y
+  }
+
+  def subtracao (x:Int, y:Int): Int ={
+    x -y
+  }
+
+  def main (args: Array[String])={
+    Calc(soma,5,3)
+    Calc(divisao,5,3)
+    Calc(multiplicacao,5,3)
+    Calc(subtracao,5,3)
+  }
+
+}
